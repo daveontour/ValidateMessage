b70c02935efd2252ce9bbfc590f2a126eb227ffd
Dave Burton <dave_on_tour@yahoo.com>
2022-10-19 12:26:19 +0400

    Add project files.

---
+++ ValidateMessage/ExtensionMethods.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+
+namespace ValidateMessage
+{
+
+    public static class ExtensionMethods
+    {
+
+        public static string PrintXML(this string xml)
+        {
+            string result = "";
+
+            MemoryStream mStream = new MemoryStream();
+            XmlTextWriter writer = new XmlTextWriter(mStream, Encoding.Unicode);
+            XmlDocument document = new XmlDocument();
+
+            try
+            {
+                // Load the XmlDocument with the XML.
+                document.LoadXml(xml);
+
+                writer.Formatting = Formatting.Indented;
+
+                // Write the XML into a formatting XmlTextWriter
+                document.WriteContentTo(writer);
+                writer.Flush();
+                mStream.Flush();
+
+                // Have to rewind the MemoryStream in order to read
+                // its contents.
+                mStream.Position = 0;
+
+                // Read MemoryStream contents into a StreamReader.
+                StreamReader sReader = new StreamReader(mStream);
+
+                // Extract the text from the StreamReader.
+                string formattedXml = sReader.ReadToEnd();
+
+                result = formattedXml;
+            }
+            catch (Exception)
+            {
+                // NO-OP
+            }
+
+            mStream.Close();
+            writer.Close();
+            return result;
+        }
+    }
+}
---
+++ ValidateMessage/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+
+namespace ValidateMessage
+{
+    internal class Program
+    {
+        static string xml = @"<aidx:IATA_AIDX_FlightLegRQ
+            xmlns:aidx=""http://www.iata.org/IATA/2007/00"" 
+            CorrelationID=""Oct202210101326testabc1""
+            SequenceNmbr=""0""
+            Target=""Production""
+            TimeStamp=""2022-10-10T10:14:24Z""
+            TransactionIdentifier=""QREIX"" Version=""16.1""
+            xmlns:aip=""http://www.sita.aero/aip/XMLSchema"">  
+            <aidx:TPA_Extension>
+                        <aip:FlightDataCondition>
+                                    <aip:FlightIDCondition>
+                                                <aip:STOCondition>
+                                                            <aip:Between>
+                                                                        <aip:RangeFrom>2022-10-10T00:00:00+03:00</aip:RangeFrom>
+                                                                        <aip:RangeTo>2022-10-10T23:59:00+03:00</aip:RangeTo>
+                                                            </aip:Between>
+                                                </aip:STOCondition>
+                                    </aip:FlightIDCondition>
+                        </aip:FlightDataCondition>
+            </aidx:TPA_Extension>
+</aidx:IATA_AIDX_FlightLegRQ>
+
+";
+        static void Main(string[] args)
+        {
+            try
+            {
+                XDocument xmlDocument = XDocument.Parse(xml);
+  
+
+                Console.WriteLine("Message XML: \n\n");
+                Console.WriteLine(xmlDocument.ToString().PrintXML());
+
+                XmlSchemaSet schemas = new XmlSchemaSet();
+                schemas.Add("http://www.iata.org/IATA/2007/00", @"XSD\IATA_AIDX_FlightLegRQ.xsd");
+                schemas.Add("http://www.sita.aero/aip/XMLSchema", @"XSD\AIPComplexTypes.xsd");
+                schemas.Add("http://www.sita.aero/aip/XMLSchema", @"XSD\AIPSimpleTypes.xsd");
+
+                bool errors = false;
+                xmlDocument.Validate(schemas, (o, e) =>
+                {
+                    Console.WriteLine("{0}", e.Message);
+                    errors = true;
+                });
+                if (!errors) Console.WriteLine("\nXML Sample validated");
+                if (errors) Console.WriteLine("\nXML Sample did not validated");
+
+                Console.WriteLine("\nPress Any Key to Exit");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Debug.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
